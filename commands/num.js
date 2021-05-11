b3bfce3cdda79ba5ab4a097c0150e5d5d4e350e8
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-11 22:28:18 +0530

    num command for number trivia

---
+++ commands/num.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+async function num(msg, args){
+    let numb;
+    let numurl;
+
+    if(args.length > 0){
+        for(const arg of args){
+            if(parseInt(arg)){
+                numb = parseInt(arg);
+                numurl = `http://numbersapi.com/${numb}/trivia?json`;
+                break;
+            }
+        }
+        if(!numb){
+            msg.reply('Provide a valid number!');
+            return;
+        }
+    }
+    else{
+        numurl = `http://numbersapi.com/random/trivia?json`;
+    }
+    
+    let response = await fetch(numurl);
+    let data = await response.json();
+    if(data.found){
+        msg.channel.send(data.text);
+    }
+    else if(!data.found){
+        msg.reply(`No trivia associated with ${numb}. Try a different number.` );
+    }
+}
+
+module.exports = num;
