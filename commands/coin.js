26f2f78c4e5b28d310b00fc9b36360ac8036f504
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-12 22:10:04 +0530

    coin command: flip a coin for you

---
+++ commands/coin.js
@@ -0,0 +1,7 @@
+function coin(msg, args){
+    let ht = Math.floor(Math.random()*2);
+    let choice = ht ? 'Heads it is!' : 'It\'s a Tail.';
+    msg.reply(`\`${choice}\``);
+}
+
+module.exports = coin;
