578f343162328ccd3b6a7aaa8d424a50cdba2a6a
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-13 21:27:17 +0530

    updated avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,14 +1,14 @@
 const Discord = require('discord.js');
 
 function avatar(msg, args){
-    if(!msg.mentions.members.first()){
+    if(!msg.mentions.members || !msg.mentions.members.first()){
         const attachment = new Discord.MessageAttachment(msg.author.displayAvatarURL());
         msg.channel.send(`${msg.author} Your Avatar:`, attachment);
     }
     else{
         const mention = msg.mentions.members.first().user;
         const attachment = new Discord.MessageAttachment(mention.displayAvatarURL());
-        msg.channel.send(`${mention.username}'s avatar:`, attachment);
+        msg.channel.send(`<@${mention.id}>'s avatar:`, attachment);
     }
 }
 
