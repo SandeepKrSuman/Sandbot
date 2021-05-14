c45241751e09c9606b19fda7d38c0653012f167e
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-14 15:50:42 +0530

    modified cmd.js

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,31 +1,30 @@
-function cmd(msg, args){
+function cmd(msg){
+    
     const embdmsg = {
         color: 0x0099ff,
         title: 'Sandbot Commands',
         url: 'https://github.com/SandeepKrSuman/Sandbot/blob/master/commands.md',
-        description: 'Available commands for the Sandbot.',
+        description: `Available commands for the Sandbot. \n\n\`\`\`\n ${availablecmds()} \n\`\`\``,
         thumbnail: {
             url: `${msg.author.displayAvatarURL()}`,
         },
-        fields: [
-            {name: '\u200B', value: '\u200B'},
-            {name: '!ping', value: '```\n ping the bot using !ping \n```'},
-            {name: '!greet', value: '```\n make the bot greet you. \n```'},
-            {name: '!gif', value: '```\n sends a gif. \n```'},
-            {name: '!num', value: '```\n trivia of a number. \n```'},
-            {name: '!avatar', value: '```\n sends the requested avatar. \n```'},
-            {name: '!cmd', value: '```\n view all the available bot commands. \n```'},
-            {name: '!info', value: '```\n info of the server/user. \n```'},
-            {name: '!coin', value: '```\n flips a coin for you. \n```'},
-            {name: '!choose', value: '```\n make a selection from your options. \n```'},
-            {name: '!img', value: '```\n sends random image. \n```'},
-            {name: '\u200B', value: '\u200B'},
-        ],
         footer: {
             text: 'click on Sandbot Commands above for detailed explaination of the commands.'
         }
     };
     msg.channel.send({ embed: embdmsg });
+}
+
+function availablecmds(){
+    const fs = require('fs');
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    let ctr = 0;
+    let cmdstr='';
+    for(let i=0; i<commandFiles.length; i++){
+        ctr = ctr+1;
+        cmdstr = cmdstr + `${ctr%4===0 ? '\n\n' : ctr===1 ? '': '\t'}` + '!' + commandFiles[i].substring(0,commandFiles[i].length-3);
+    }
+    return cmdstr;
 }
 
 module.exports = cmd;
