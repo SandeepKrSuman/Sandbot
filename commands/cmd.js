0ead15179af81eef365cb9f22a580b9c231a34ce
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-15 21:28:57 +0530

    updated cmd

--- commands/cmd.js
+++ commands/cmd.js
@@ -1,12 +1,12 @@
 function cmd(msg){
-    
+
     const embdmsg = {
         color: 0x0099ff,
         title: 'Sandbot Commands',
         url: 'https://github.com/SandeepKrSuman/Sandbot/blob/master/commands.md',
         description: `Available commands for the Sandbot. \n\n\`\`\`\n ${availablecmds()} \n\`\`\``,
         thumbnail: {
-            url: `${msg.author.displayAvatarURL()}`,
+            url: `${msg.client.user.displayAvatarURL()}`,
         },
         footer: {
             text: 'click on Sandbot Commands above for detailed explaination of the commands.'
@@ -16,13 +16,19 @@ function cmd(msg){
 }
 
 function availablecmds(){
+
     const fs = require('fs');
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+    const commands = [];
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commands.push(command.name);
+    }
     let ctr = 0;
     let cmdstr='';
-    for(let i=0; i<commandFiles.length; i++){
+    for(let i=0; i<commands.length; i++){
         ctr = ctr+1;
-        cmdstr = cmdstr + `${ctr%4===0 ? '\n\n' : ctr===1 ? '': '\t'}` + '!' + commandFiles[i].substring(0,commandFiles[i].length-3);
+        cmdstr = cmdstr + `${ctr%4===0 ? '\n\n' : ctr===1 ? '': '\t'}` + '!' + commands[i];
     }
     return cmdstr;
 }
