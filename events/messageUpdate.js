d375b5380d085072b717855eb8a6debb24be0436
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2021-05-16 21:50:56 +0530

    updated messageUpdate event

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'messageUpdate',
     execute(oldmsg, newmsg){
         if(newmsg.content.search('hide from bot') !== -1){
-            newmsg.reply(`I see you updated your old message to: \`${newmsg.content}\``);
+            newmsg.reply(`I know that your old message was: \`${oldmsg.content}\``);
         }
     }
 };
